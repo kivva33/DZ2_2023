9f0233444865d4f34adfa1af1aa50b4ac15a8000
kivva33 <kenlil@bk.ru>
2023-05-22 02:32:02 +0300

    updataSrcImg

--- hw2.cpp
+++ hw2.cpp
@@ -7,17 +7,78 @@ Img::Img(int w, int h){
     srcImg = new  int[width * height];
 }
 Img::Img( const int* src, int w, int h) : Img(w, h) {
-    for(int i = 0; i < h; i++)
-        for(int j = 0; j < w; j++){
-            srcImg[h * i + j] = src[h * i + j];
-        }
+    for(int i = 0; i < h * w; i++)
+        srcImg[i] = src[i];
 }
 Img::~Img() {
     delete[] srcImg;
 }
+int& Img::get_element(int i, int j) {
+    return srcImg[i * width + j];
+}
+/*
+ImageProcess();
+ выделяется память для картинки размера w*h
+ImageProcess(int w, int h);
+ выделяется память для картинки размера w*h и копируется картинка
+ImageProcess(const Img* img);//откуда взять w*h
+ выделяется память для картинки копируется картинка из файла, первая строчка в файле содержит ширину и высоту картинки
+ImageProcess(const char* fileName);//откуда взять w*h
+~ImageProcess();*/
+ImageProcess::ImageProcess() : ImageProcess::ImageProcess(0, 0){}
+ImageProcess::ImageProcess(int w, int h){
+    srcImg = new Img;
+    srcImg->height = h;
+    srcImg->width = w;
 
-ImageProcess::ImageProcess(){}
-ImageProcess::ImageProcess(int w, int h){}
-ImageProcess::ImageProcess(const Img* img){}
-ImageProcess::ImageProcess(const char* fileName){}
+    processedImg = new Img;
+    processedImg->width = w;
+    processedImg->height = h;
+}
+ImageProcess::ImageProcess(const Img* img) : ImageProcess::ImageProcess(img->width, img->height){
+    for(int i = 0; i < img->width * img->height; i++)
+        srcImg->srcImg[i] = img->srcImg[i];
+}
+ImageProcess::ImageProcess(const char* fileName){
+    FILE* f = fopen(fileName, "r");
+    int width, height;
+    fscanf(f, "%d%d", width, height);
+    ImageProcess(width, height);
+    for(int i = 0; i < width * height; i++)
+        fscanf(f, "%d", srcImg->srcImg[i]);
+}
 
+ImageProcess::~ImageProcess(){
+    delete srcImg;
+    delete processedImg;
+    delete mask;
+}
+//ImageProcess::
+int ImageProcess::updateMask(const Img& mask){
+    this->mask->width = mask.width;
+    this->mask->height = mask.height;
+    this->mask->x_c = mask.x_c;
+    this->mask->y_c = mask.y_c;
+    for(int i = 0; i < mask.width * mask.height; i++)
+        this->mask->srcImg[i] = mask.srcImg[i];
+}
+int ImageProcess::updateSrcImg(){
+    if(processedImg->width == srcImg->width && processedImg->height == srcImg->height) {
+        Img *avarage;
+        avarage = srcImg;
+        srcImg = processedImg;
+        processedImg = avarage;
+        return 0;
+    }
+    return -1;
+}
+
+int ImageProcess::dilatation(int srcImg){
+
+}
+
+int ImageProcess::erosion(int srcImg){
+
+}
+int loadImgFromFile(const char* fileName, int format = 1);// читаем из файла
+int saveImgToFile(const char* fileName, int format = 1);//записываем в файл
--- hw2.h
+++ hw2.h
@@ -1,4 +1,5 @@
 #include "list"
+#include <fstream>
 
 struct Img {
         int* srcImg;
@@ -7,7 +8,7 @@ struct Img {
 
         int x_c;//координаты центра, с которыми сравниваем
         int y_c;
-
+        int& get_element(int i, int j);//функция, возвращающая элемент
         Img();
         Img(int w, int h);
         Img( const int* src, int w, int h);
@@ -19,37 +20,41 @@ private :
     Img* srcImg;
     Img* processedImg;
     Img* mask;
-    //здесь можно объявлять другие переменные и функции
 public :
     ImageProcess();
     /* выделяется память для картинки размера w*h */
     ImageProcess(int w, int h);
     /* выделяется память для картинки размера w*h и копируется картинка*/
-    ImageProcess(const Img* img);
+    ImageProcess(const Img* img);//откуда взять w*h
     /* выделяется память для картинки копируется картинка из файла, первая строчка в файле содержит ширину и высоту картинки*/
-    ImageProcess(const char* fileName);
+    ImageProcess(const char* fileName);//откуда взять w*h
     ~ImageProcess();
 
     /*задание маски*/
     int updateMask(const Img& mask);
+
     /*перезаписать исходную картинку картинкой, которая была получена в результате дилатации и/или эрозии*/
     int updateSrcImg();
+
     /*дилатация картинки, результат записать в processedImg*/
     /*
         input		:	srcImg = 1, обрабатывается srcImg  - результат записывается в processedImg
                         srcImg = 0, обрабатывается processedImg - результат записывается в processedImg
     */
-    int dilotation(int srcImg = 1);	//исправить на dilatation
+    int dilatation(int srcImg = 1);
+
     /*эрозия картинки, результат записать в processedImg*/
     int erosion(int srcImg = 1);
+
     /*
         загрузить/сохранить из файла с имененм fileName картинку
         input		:	fileName - имя файла
                         format - формат файла 	0 - последовательность 0 и 1, без перехода на новую строку
                                                 1 - последовательность 0 и 1, с переходом на новую строку
     */
-    int loadImgFromFile(const char* fileName, int format = 1);
-    int saveImgToFile(const char* fileName, int format = 1);
+    int loadImgFromFile(const char* fileName, int format = 1);// читаем из файла
+    int saveImgToFile(const char* fileName, int format = 1);//записываем в файл
+
 
     /*на зачёт без зачёта*/
     std::list<std::list<std::pair<int /*x*/,int /*y*/>>> getListContours();
